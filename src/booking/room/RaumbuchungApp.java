d340ff942af298e5902bd7bec7efdef3570b7d63
9Jjzok <102530521+9Jjzok@users.noreply.github.com>
2022-05-25 14:00:46 +0200

    neueste Aenderungen an Controller und App

--- src/booking/room/RaumbuchungApp.java
+++ src/booking/room/RaumbuchungApp.java
@@ -1,32 +1,46 @@
 package booking.room;
 
-import edu.damago.java.fx.article.RootPaneController;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class RaumbuchungApp extends Application {
+	static private final String PACKAGE_PATH = RaumbuchungApp.class.getPackage().getName().replace('.', '/');
+	static private final String ICON_PATH = PACKAGE_PATH + "/raumbuchung.png";
+	static private final String CSS_PATH = PACKAGE_PATH + "/raumbuchung.css";
 	static private final int WINDOW_WIDTH = 800;
 	static private final int WINDOW_HEIGHT = 600;
 	
 	private RaumbuchungController raumbuchungController;
 
-	public void start (Stage primaryStage) throws Exception {
+	public void start (Stage window) throws Exception {
 		final BorderPane rootPane = new BorderPane();
 		this.raumbuchungController = new RaumbuchungController(rootPane);
 		
+		final Scene sceneGraph = new Scene(rootPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+		// sceneGraph.getStylesheets().add(CSS_PATH);
+
+		window.setScene(sceneGraph);
+		window.setTitle("Article App");
+		window.getIcons().add(new Image(ICON_PATH.toString()));
+		window.show();
+
+		this.raumbuchungController.registerEventHandlers();
 	}
 	
 	@Override
 	public void stop () throws Exception {
 		// close resources
-		this.RaumbuchungController.getJdbcConnection().close();
+		this.raumbuchungController.getJdbcConnection().close();
 	}
 	
 	public static void main (String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
+	
+	
 
 	
 
--- src/booking/room/RaumbuchungController.java
+++ src/booking/room/RaumbuchungController.java
@@ -1,11 +1,37 @@
 package booking.room;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import javafx.scene.layout.BorderPane;
 
 public class RaumbuchungController {
 	
-	public RaumbuchungController (final BorderPane rootPane) {
+	private final Connection jdbcConnection;
+	
+	public RaumbuchungController (final BorderPane rootPane) throws SQLException {
+		this.jdbcConnection = newDbConfig().getConnection();
+	}
+	
+	public Connection getJdbcConnection () {
+		return this.jdbcConnection;
+	}
+	
+	protected void registerEventHandlers () {
 		
 	}
 	
+	
+	
+	
+	
+	static private DataSource newDbConfig () {
+		final MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setURL("jdbc:mysql://localhost:3306/rabu?characterEncoding=UTF-8");
+		dataSource.setUser("root");
+		dataSource.setPassword("Passw0rd");
+		return dataSource;
+	}
 }
+
