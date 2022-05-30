8ce4bae85120246ed5827a1a26567afe005cf1dc
9Jjzok <102530521+9Jjzok@users.noreply.github.com>
2022-05-30 12:33:54 +0200

    fxml hinzugefügt

--- src/booking/room/RaumbuchungController.java
+++ src/booking/room/RaumbuchungController.java
@@ -1,12 +1,15 @@
 package booking.room;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 import javax.sql.DataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 
-public class RaumbuchungController {
+public class RaumbuchungController implements Initializable {
 	
 	private final Connection jdbcConnection;
 	
@@ -22,16 +25,21 @@ public class RaumbuchungController {
 		
 	}
 	
-	
-	
-	
-	
 	static private DataSource newDbConfig () {
 		final MysqlDataSource dataSource = new MysqlDataSource();
 		dataSource.setURL("jdbc:mysql://localhost:3306/rabu?characterEncoding=UTF-8");
 		dataSource.setUser("root");
 		dataSource.setPassword("Passw0rd");
 		return dataSource;
+	}
+
+	@FXML 
+	
+	
+	@Override
+	public void initialize (URL location, ResourceBundle resources) {
+	
+		
 	}
 }
 
