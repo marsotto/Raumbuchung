a6efa7f07573168c3b0b585143bf85afbaa91346
9Jjzok <102530521+9Jjzok@users.noreply.github.com>
2022-06-01 08:33:19 +0200

    weitere Design aenderungen

--- src/booking/room/RaumbuchungApp.java
+++ src/booking/room/RaumbuchungApp.java
@@ -1,10 +1,14 @@
 package booking.room;
 
+import java.util.Locale;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -16,15 +20,17 @@ public class RaumbuchungApp extends Application {
 	static private final String PACKAGE_PATH = RaumbuchungApp.class.getPackage().getName().replace('.', '/');
 	static private final String ICON_PATH = PACKAGE_PATH + "/raumbuchung.png";
 	static private final String CSS_PATH = PACKAGE_PATH + "/raumbuchung.css";
-	static private final int WINDOW_WIDTH = 800;
-	static private final int WINDOW_HEIGHT = 600;
+	static private final int WINDOW_WIDTH = 850;
+	static private final int WINDOW_HEIGHT = 650;
 	
 	private RaumbuchungController raumbuchungController;
+	
 
 	public void start (Stage window) throws Exception {
-		
+		Locale.setDefault(Locale.Category.FORMAT, Locale.GERMAN);
 		final BorderPane rootPane = newRootPane();
 		this.raumbuchungController = new RaumbuchungController(rootPane);
+		
 
 		final Scene sceneGraph = new Scene(rootPane, WINDOW_WIDTH, WINDOW_HEIGHT);
 		// sceneGraph.getStylesheets().add(CSS_PATH);
@@ -49,7 +55,7 @@ public class RaumbuchungApp extends Application {
 	
 	private BorderPane newRootPane() {
 		BorderPane rootPane = new BorderPane();
-		VBox rowBox = new VBox();
+		VBox tableViewBox = new VBox();
 		VBox topVBox = new VBox();
 		//rowBox.setPrefWidth(Integer.MAX_VALUE);
 		//rowBox.setStyle("-fx-alignment: center");
@@ -62,12 +68,10 @@ public class RaumbuchungApp extends Application {
 		ImageView logo = new ImageView(new Image(RaumbuchungApp.class.getResourceAsStream("damago_logo.png")));
 		topPane.getChildren().add(logo);
 		Label raumbuchung = new Label("Raumbuchung");
-		raumbuchung.setStyle("-color: #FFFFFF");
+		
+		raumbuchung.setStyle("-fx-text-background-color: white;" + "-fx-font-size: 20;");
 		topPane.getChildren().add(raumbuchung);
 		
-		Button zeigeButton = new Button("Zeige Buchungen");
-		zeigeButton.setPrefWidth(Integer.MAX_VALUE);
-		zeigeButton.setPrefHeight(40);
 		Button bucheButton = new Button("Buchen");
 		bucheButton.setPrefWidth(Integer.MAX_VALUE);
 		bucheButton.setPrefHeight(40);
@@ -77,7 +81,16 @@ public class RaumbuchungApp extends Application {
 		Button loescheButton = new Button("Löschen");
 		loescheButton.setPrefWidth(Integer.MAX_VALUE);
 		loescheButton.setPrefHeight(40);
-		controlPane.getChildren().addAll(zeigeButton, bucheButton, aendernButton, loescheButton);
+		
+		DatePicker von = new DatePicker();
+		von.setShowWeekNumbers(true);
+		von.setPrefWidth(Integer.MAX_VALUE);
+		von.setPrefHeight(40);
+		DatePicker bis = new DatePicker();
+		bis.setPrefWidth(Integer.MAX_VALUE);
+		bis.setPrefHeight(40);
+		
+		controlPane.getChildren().addAll(bucheButton, aendernButton, loescheButton, von, bis);
 		
 		topVBox.getChildren().addAll(topPane, controlPane);
 		
@@ -87,8 +100,19 @@ public class RaumbuchungApp extends Application {
 		
 		HBox bottomPane = new HBox(refreshButton);
 		
+		TableView buTabelle = new TableView();
+		buTabelle.setEditable(true);
+		TableColumn id_buchung = new TableColumn("ID Buchung");
+		TableColumn id_klasse = new TableColumn("ID Klasse");
+		TableColumn id_raum = new TableColumn("ID Raum");
+		TableColumn datum_von = new TableColumn("Von");
+		TableColumn datum_bis = new TableColumn("Bis");
+		buTabelle.getColumns().addAll(id_buchung, id_klasse, id_raum, datum_von, datum_bis);
+		
+		tableViewBox.getChildren().add(buTabelle);
+		
 		rootPane.setTop(topVBox);
-		rootPane.setCenter(rowBox);
+		rootPane.setCenter(tableViewBox);
 		rootPane.setBottom(bottomPane);
 
 		return rootPane;
